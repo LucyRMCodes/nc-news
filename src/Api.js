63fc7da2467e6bef9262f8fc893c2e0ef5ad650f
LucyRTCodes <lucyrtutton@gmail.com>
2023-07-17 15:17:46 +0100

    begin ticket 6

--- src/Api.js
+++ src/Api.js
@@ -1,11 +1,17 @@
 import axios from "axios";
 
 const newsApi = axios.create({
-	baseUrl: "https://lucy-nc-news.onrender.com/api",
+	baseURL: "https://lucy-nc-news.onrender.com/api",
 });
 
 export const fetchArticles = () => {
 	return axios.get("/articles").then(({ data }) => {
+		return data.articles;
+	});
+};
+
+export const fetchComments = (articleId) => {
+	return axios.get(`/articles/${articleId}/comments`).then(({ data }) => {
 		return data.articles;
 	});
 };
