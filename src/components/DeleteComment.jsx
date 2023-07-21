5ccc9b7a390bc17d569776a36f6849b39a31b9b2
LucyRTCodes <lucyrtutton@gmail.com>
2023-07-21 10:09:21 +0100

    add error handling

--- src/components/DeleteComment.jsx
+++ src/components/DeleteComment.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import { deleteComment } from "../Api";
 
 function DeleteComment({ setComments, commentId }) {
+	const [error, setError] = useState(null);
 	const handleClick = () => {
-		deleteComment(commentId).then(() => {
-			setComments((current) => {
-				return current.filter((comment) => {
-					return comment.comment_id !== commentId;
+		deleteComment(commentId)
+			.then(() => {
+				setComments((current) => {
+					return current.filter((comment) => {
+						return comment.comment_id !== commentId;
+					});
 				});
+			})
+			.catch(() => {
+				setError("Could not delete");
 			});
-		});
 	};
+
+	if (error) return <p style={{ color: "#eb1c24" }}>{error}</p>;
 
 	return (
 		<button
